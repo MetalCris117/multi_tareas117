7e9a642082a3bb80c64e6df751009c12143b07ad
Kaedee2 <gosh728714@gmail.com>
2025-11-28 16:14:42 -0600

    Actualizando main con pruebas

---
+++ multi_tareas117/src/main/java/com/mycompany/multi_tareas117/Multi_tareas117.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.multi_tareas117;
+
+/**
+ *
+ * @author shiro
+ */
+public class Multi_tareas117 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
